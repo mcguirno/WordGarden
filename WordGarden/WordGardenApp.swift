b57e8c3ee4c0b82907a9014581540ac2a2df0ce7
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-09 16:32:03 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Noah McGuire on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
